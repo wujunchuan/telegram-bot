0d0cacd7a841db97b6397aaaaa577c3255bd2f98
John Trump <wujunchuan2008@gmail.com>
2021-07-25 10:25:32 +0000

    feat: 接入 GitHub

--- api/webhook.ts
+++ api/webhook.ts
@@ -4,18 +4,10 @@ process.env.NTBA_FIX_319 = "test";
 
 import TelegramBot from "node-telegram-bot-api";
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import costflow from "costflow";
-import { NParseResult } from "costflow/lib/interface";
+import { parseCostflow } from "../lib/cosflow";
+import { appendToBeancountContent } from "../lib/octo";
 
-const config = {
-  mode: "beancount" as const,
-  currency: "CNY",
-  timezone: "Asia/Hong_Kong",
-  account: {
-    信用卡: "Liabilities:CreditCard:CMB",
-    捐赠: "Expenses:Blog:Donate",
-  },
-};
+const AUTHED_ACCOUNT = "goh_john_trump";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   if (!process.env.TELEGRAM_TOKEN) throw new Error("TELEGRAM_TOKEN Undefined.");
@@ -26,25 +18,35 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     const { message } = body;
     if (message) {
       const {
-        chat: { id },
+        chat: { id, username },
         message_id,
         text = "",
       } = message as TelegramBot.Message;
 
+      /* 简单的鉴权 */
+      if (AUTHED_ACCOUNT !== username) {
+        await bot.sendMessage(id, "滚犊子", {
+          reply_to_message_id: message_id
+        });
+        throw new Error("非本人调用");
+      };
+
+      /* 解析成 Beancount 语法 */
       try {
-        const { output } = await costflow.parse(text, config) as NParseResult.Result;
+        const output = await parseCostflow(text);
+        await appendToBeancountContent(output!, text);
         await bot.sendMessage(id, output!, { reply_to_message_id: message_id });
       } catch (e) {
-        bot.sendMessage(id, e.message, {
+        bot.sendMessage(id, `解析错误：${e.message}`, {
           reply_to_message_id: message_id,
         });
       }
+
     }
   } catch (error) {
     // If there was an error sending our message then we
     // can log it into the Vercel console
-    console.error("Error sending message");
-    console.log(error.toString());
+    console.error(error.toString());
   }
   response.send("祖宗之法不可变，五瓦为体，快充为用");
 };
